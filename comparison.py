dacbe921d033ccb5c5a0d97a0d5e10bfc1b274f4
YoolutiPhalyngki <lutiyoo@rediffmail.com>
2021-08-15 10:42:29 +0530

    comparison operators

---
+++ comparison.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+
+# Comparisons:
+# Equal: 3 == 2
+# Not Equal: 3 != 2
+# Greater Than: 3 > 2
+# Less Than: 3 < 2
+# Greater or Equal: 3 >= 2
+# Less or Equal: 3 <= 2
+
+num_1 = 3
+num_2 = 2
+
+print(num_1 == num_2)
