69e8060f6e5413c834063c0973b6bfbf5bbd0730
YoolutiPhalyngki <lutiyoo@rediffmail.com>
2021-08-15 12:16:18 +0530

    convert string to list using split method

--- list_to_string.py
+++ list_to_string.py
@@ -2,6 +2,9 @@
 
 courses = ['History', 'Math', 'Physics', 'CompSci']
 
-course_str = ', '.join(courses)
+course_str = ' - '.join(courses)
+
+new_list = course_str.split(' - ')
 
 print(course_str)
+print(new_list)
