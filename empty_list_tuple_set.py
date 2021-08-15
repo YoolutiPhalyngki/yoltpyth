dcf425e0afb7a6785b69e61f87b7d55e7e71feb0
YoolutiPhalyngki <lutiyoo@rediffmail.com>
2021-08-15 17:16:56 +0530

    initialize an empty list or tuple or set

---
+++ empty_list_tuple_set.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# Empty Lists
+empty_list = []
+empty_list = list()
+
+# Empty Tuples
+empty_tuple = ()
+empty_tuple = tuple()
+
+# Empty Sets
+empty_set = {} # This isn't right! It's a dict
+empty_set = set()
+
