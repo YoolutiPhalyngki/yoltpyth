4bb3ca6f61cab9a84de86bef3e3f77de38247ebb
YoolutiPhalyngki <lutiyoo@rediffmail.com>
2021-08-15 17:11:49 +0530

    combine all item in 2 set

--- set.py
+++ set.py
@@ -4,4 +4,4 @@
 cs_courses = {'History', 'Math', 'Physics', 'CompSci'}
 art_courses = {'History', 'Math', 'Art', 'Design'}
 
-print(cs_courses.difference(art_courses))
+print(cs_courses.union(art_courses))
