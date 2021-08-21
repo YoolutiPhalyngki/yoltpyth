80dcbc1e893d64e186d9f48adc67031823abee24
YoolutiPhalyngki <lutiyoo@rediffmail.com>
2021-08-21 22:42:54 +0530

    example of dictionary in python

---
+++ dict.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+student = {'name': 'John', 'age': 25, 'courses': ['Math', 'CompSci']}
+
+student.update({'name': 'Jane', 'age': 26, 'phone': '555-5555'})
+
+# print(student.get('phone', 'Not Found'))
+print(student)
