be6bf9f7f58d195706759b05b0a8b8ca2ee74c25
YoolutiPhalyngki <lutiyoo@rediffmail.com>
2021-08-22 12:34:10 +0530

    value evaluate to false and otherwise will evaluate to true

---
+++ true_false_evaluate.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# False Values:
+    # False
+    # None
+    # Zero of any numeric type
+    # Any empty sequence. For example, '', (), [].
+    # Any empty mapping. For example, {}.
+
+condition = {}
+
+if condition:
+    print('Evaluated to True')
+else:
+    print('Evaluate to False')
