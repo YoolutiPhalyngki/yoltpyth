5eebc903deac7826012e3bc99d7f914e8f3c35ec
YoolutiPhalyngki <lutiyoo@rediffmail.com>
2021-08-22 12:24:16 +0530

    is in conditional statement

---
+++ object_identity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# Comparisons:
+# Equal: ==
+# Not Equal: !=
+# Greater Than: >
+# Less Than: <
+# Greater or Equal: >=
+# Less or Equal: <=
+# Object Identity: is
+
+a = [1, 2, 3]
+b = [1, 2, 3]
+
+print(a == b)
+print(a is b)
